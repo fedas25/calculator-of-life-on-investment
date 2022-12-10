7962bb68a139dbcd5fab981216ec48a06a0502a4
Fedor <uasily.lol@yandex.ru>
2022-12-11 02:09:02 +0300

    add comments

--- main.js
+++ main.js
@@ -12,50 +12,50 @@ const equation = {
     requiredAmountMoneyInYear: null,
     coefficientXForAccumulation: 1.04, // поменять, чтобы -1 не делать с 0 начинать ?
     amountDeferralYear: null,
-
-    calculationOfYearsContribution() { // непонятки с годами
-        for (let i = 0; i < this.saveYears - 1; i++) {
-           this.stepCalculationOfYearsContribution();
-        }
-      this.amountDeferralYear = Math.ceil(this.requiredAmountMoneyInYear / this.coefficientXForAccumulation);
-    },
-
-    stepCalculationOfYearsContribution() { // сколько нужно в месяц
-        this.coefficientXForAccumulation += this.coefficientXForAccumulation + ( this.coefficientXForAccumulation * 0.04 );
-    },
-    
-    setValuesFrom() {
-        this.retiredYears = dateDeath.value - beginningRetirement.value;
-        this.coefficientY = amountMoneyPerYear.value * this.retiredYears; // не та сумма
-        this.saveYears = beginningRetirement.value - startOfInvestment.value;
-    },
-
-    calculateCoefficientsInStep() { // сколько нужно cчитает год
-        this.coefficientX = this.coefficientX + ( this.coefficientX * 0.04 );
-        this.coefficientY = this.coefficientY + ( this.coefficientY * 0.04 ) + 100;
-    },
-
-    calculateYearlyContribution(){ // сколько нужно накопить работатет от 2 лет
-        message.innerText = "";  // разбить на функции
-
-        for (let i = 0; i < this.retiredYears - 1; i++) {
-            this.calculateCoefficientsInStep();
-        }
-
-        this.requiredAmountMoneyInYear = this.coefficientY / this.coefficientX; // на всю жизнь
-        
-        this.calculationOfYearsContribution();
-        // console.log(this.coefficientX, this.coefficientY, this.coefficientY / this.coefficientX);
-        
-        message.innerText = `${Math.ceil(this.requiredAmountMoneyInYear)} нужно для жизни на пенсии ${this.retiredYears} лет /n
-        откладывая по ${this.amountDeferralYear} в год на протяжении ${this.saveYears}`;
-        [this.retiredYears, this.coefficientY, this.requiredAmountMoneyInYear, this.coefficientX] = [null, null, null, 1.04] 
-    }
+    retirementSize: null
 }
+    // calculationOfYearsContribution = () => { // непонятки с годами
+    //     for (let i = 0; i < equation.saveYears - 1; i++) {
+    //        stepCalculationOfYearsContribution();
+    //     }
+    //   equation.amountDeferralYear = Math.ceil(equation.requiredAmountMoneyInYear / equation.coefficientXForAccumulation);
+    // }
+
+    // stepCalculationOfYearsContribution = () => { // сколько нужно в месяц
+    //     equation.coefficientXForAccumulation += equation.coefficientXForAccumulation + ( equation.coefficientXForAccumulation * 0.04 );
+    // }
+    
+    setValuesFrom = () => {
+        equation.retiredYears = Number(dateDeath.value - beginningRetirement.value);
+        equation.coefficientY = Number(amountMoneyPerYear.value);
+        equation.retirementSize = Number(equation.coefficientY) ;
+        equation.saveYears = Number(beginningRetirement.value - startOfInvestment.value);
+    }
+// coefficientX   coefficientX   retirementSize
+    calculateCoefficientsInStep = () => {
+        equation.coefficientX = equation.coefficientX + ( equation.coefficientX * 0.04 );
+        equation.coefficientY = equation.coefficientY + ( equation.coefficientY * 0.04 ) + equation.retirementSize;
+    }
+
+    calculateYearlyContribution = () => { // сколько нужно накопить работатет от 2 лет // разбить на функции
+        message.innerText = "";
+
+        for (let i = 0; i < equation.retiredYears - 1; i++) {
+            calculateCoefficientsInStep();
+        }
+
+        equation.requiredAmountMoneyInYear = equation.coefficientY / equation.coefficientX; // на всю жизнь
+        
+        // calculationOfYearsContribution();
+        // console.log(equation.coefficientX, equation.coefficientY, equation.coefficientY / equation.coefficientX);
+        
+        message.innerText = `${Math.ceil(equation.requiredAmountMoneyInYear)} нужно для жизни на пенсии ${equation.retiredYears} лет`;
+        [equation.retiredYears, equation.coefficientY, equation.requiredAmountMoneyInYear, equation.coefficientX] = [null, null, null, 1.04] 
+    }
 
 document.addEventListener("keydown", (e) => {
     if (e.code === "Enter") {
-        equation.setValuesFrom();
-        equation.calculateYearlyContribution();
+        setValuesFrom();
+        calculateYearlyContribution();
     }
 })
